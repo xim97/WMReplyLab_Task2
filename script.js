ba645e0fbe0341b824c306aaeb158f076ea66e82
Maxim Yefimovich <xim97@mail.ru>
2018-10-01 23:52:52 +0300

    added classes to work app

--- script.js
+++ script.js
@@ -1,76 +1,82 @@
-/*fetch("http://api.giphy.com/v1/gifs/search?api_key=y75ufo1GAk6BPtV5TPggZMw5b0HNeFtQ&q=todd%20howard")
-    .then(respone => respone.json()).then(data => {
-        console.log(data);
-
-    }
-    );
-document.getElementsByClassName("gif")[document.getElementsByClassName("gif").length - 1].offsetHeight
-*/
+const api_key = "kqCPXa4qvrRlHSS8vyYPNeROW9HsO4K1";
 const searchInput = document.getElementById("search-input");
-const api_key = "y75ufo1GAk6BPtV5TPggZMw5b0HNeFtQ";
 const content = document.getElementById("content");
 const dropDown = document.getElementById("dropdown");
-let resultOfRequest;
-let offset = 0, isLoading = false, section = "gifs";
-let storage = JSON.parse(localStorage.getItem("favourites")) || [];
-if (storage.length > 0) {
-    let section = document.getElementById("pick-section");
-    section.appendChild(createFavouriteRadioButton());
-    document.getElementsByName("contentType").
-        forEach(element =>
-            element.addEventListener("change", handleChangePickContentType));
-}
-searchInput.addEventListener("input", handleInputChange);
-document.getElementById("search-button").addEventListener("click", handleClickSearchButton);
-document.getElementsByName("contentType").
-    forEach(element =>
-        element.addEventListener("change", handleChangePickContentType));
 
-window.addEventListener("click", handleClickNotOnDropDown);
+let resultOfRequest,
+    offset = 0,
+    isLoading = false,
+    section = "gifs",
+    storage = JSON.parse(localStorage.getItem("favourites")) || [];
 
-function handleClickNotOnDropDown(event) {
-    if (event.target != dropDown) {
-        removeDataIfExistFromDropDown();
-    }
-}
+class Utils {
 
+    static checkForNewDiv() {
+        if (!isLoading && content.children.length > 0) {
+            let lastDiv = document.querySelector("#content > div:last-child"),
+                lastDivOffset = lastDiv.offsetTop + lastDiv.clientHeight,
+                pageOffset = window.pageYOffset + window.innerHeight;
 
-window.addEventListener("scroll", () => {
-    if (!isLoading) {
-        checkForNewDiv();
-    }
-});
-
-function handleInputChange(event) {
-    const request = event.target.value;
-    removeDataIfExistFromDropDown();
-    findDataForDropDown(request);
-}
-
-function createFavouriteRadioButton() {
-    let label = document.createElement("label");
-    label.innerHTML = "<input type=\"radio\" id=\"favouriteButton\" name=\"contentType\" value=\"favourite\" />Избранное";
-    return label;
-}
-
-function handleClickFavoriteButton(event) {
-    const targetItem = event.target.parentNode.parentNode;
-    searchInput.focus();
-    if (event.target.classList.contains("far")) {
-        event.target.classList.remove("far");
-        event.target.classList.add("fas");
-        addItemToStorage(targetItem);
-        if (storage.length > 0 && !document.getElementById("favouriteButton")) {
-            let section = document.getElementById("pick-section");
-            section.appendChild(createFavouriteRadioButton());
-            document.getElementsByName("contentType").
-                forEach(element =>
-                    element.addEventListener("change", handleChangePickContentType));
+            if (pageOffset > lastDivOffset - 500) {
+                offset++;
+                Utils.getData(searchInput.value, "content");
+            }
         }
-    } else {
-        event.target.classList.remove("fas");
-        event.target.classList.add("far");
-        deleteItemFromStorage(targetItem);
+    }
+
+    static getParentNode(element) {
+        let currentNode = element;
+        while (currentNode.parentNode != content &&
+            currentNode.parentNode != dropDown &&
+            currentNode.parentNode != document) {
+            currentNode = currentNode.parentNode
+        }
+        return currentNode.parentNode;
+    }
+
+    static getData(request, place) {
+        if (section != "favourite") {
+            isLoading = true;
+            fetch(`http://api.giphy.com/v1/${section}/search?api_key=${api_key}&q=${request}&limit=${place == "content" ? offset == 0 ? 10 : 5 : 5}&offset=${offset == 0 ? 0 : 20 + (offset - 1) * 5}`)
+                .then(response => response.json()).then(response => {
+                    Creator.createListOfEntities(response.data, place);
+                    resultOfRequest = [...response.data];
+                }
+                ).then(() => isLoading = false).catch( error => {
+                    console.error(error);                    
+                });
+        } else {
+            Creator.createListOfEntities(storage.slice(5 * offset, 5 * (offset + 1)), "content");           
+        }
+
+    }
+}
+
+
+class Eraser {
+    static removeDataIfExist(place) {
+        let listOfEntities = document.querySelectorAll(`#${place} .gif`);
+        listOfEntities.forEach(element => {
+            switch (place) {
+                case "content": {
+                    content.removeChild(element);
+                    break;
+                }
+                case "dropdown": {
+                    dropDown.removeChild(element);
+                    break;
+                }
+            }
+        });
+    }
+
+    static deleteItemFromStorage(item) {
+        storage = JSON.parse(localStorage.getItem("favourites"));
+        storage = storage.filter(element => element.id != item.getAttribute("gifid"));
+        localStorage.setItem("favourites", JSON.stringify(storage));
+    }
+
+    static deleteFavouriteRadioButton() {
         if (storage.length == 0 && document.getElementById("favouriteButton")) {
             let section = document.getElementById("pick-section");
             section.removeChild(section.lastChild);
@@ -78,147 +84,165 @@ function handleClickFavoriteButton(event) {
     }
 }
 
-function addItemToStorage(item) {
-    storage.unshift({
-        id: item.getAttribute("gifid"),
-        title: item.querySelector("h5").innerText,
-        url: item.lastChild.getAttribute("src")
-    });
-    localStorage.setItem("favourites", JSON.stringify(storage));
-}
+class Adder {
 
-function deleteItemFromStorage(item) {
-    storage = JSON.parse(localStorage.getItem("favourites"));
-    storage = storage.filter(element => element.id != item.getAttribute("gifid"));
-    localStorage.setItem("favourites", JSON.stringify(storage));
-}
-
-function handleChangePickContentType(event) {
-    offset = 0;
-    section = event.target.value;
-    removeDataIfExist();
-    findData(searchInput.value);
-}
-
-function checkForNewDiv() {
-    var lastDiv = document.querySelector("#content > div:last-child");
-    var lastDivOffset = lastDiv.offsetTop + lastDiv.clientHeight;
-    var pageOffset = window.pageYOffset + window.innerHeight;
-
-    if (pageOffset > lastDivOffset - 500) {
-        offset++;
-        findData(searchInput.value);
-    }
-};
-
-function handleClickSearchButton(event) {
-    removeDataIfExistFromDropDown();
-    removeDataIfExist();
-    if (searchInput.value) {
-        findData(searchInput.value);
-    }
-}
-
-function findData(request) {
-    if (section != "favourite") {
-        isLoading = true;
-        fetch(`http://api.giphy.com/v1/${section}/search?api_key=${api_key}&q=${request}&limit=${offset == 0 ? 10 : 5}&offset=${offset == 0 ? 0 : 20 + (offset - 1) * 5}`)
-            .then(respone => respone.json()).then(response => {
-                response.data.forEach(element => {
-                    content.appendChild(createGif(element));
-                });
-            }
-            ).then(() => isLoading = false);
-    } else {
-        storage.slice(5 * offset, 5 * (offset + 1)).forEach(element => {
-            content.appendChild(createGif(element));
-        });
+    static addAllListeners() {
+        Adder.addFavouriteButtonToPickSection();
+        Adder.addEventListenerForSearchInput();
+        Adder.addEventListenerForSearchButton();
+        Adder.addEventListenersForPickSection();
+        Adder.addEventListenersForWindow();
     }
 
-}
-
-function findDataForDropDown(request) {    
-    isLoading = true;
-    fetch(`http://api.giphy.com/v1/${section != "favourite" ? section : "gifs"}/search?api_key=${api_key}&q=${request}&limit=${5}`)
-        .then(respone => respone.json()).then(response => {
-            response.data.forEach(element => {
-                dropDown.appendChild(createSmallGif(element));
-            });
+    static addFavouriteButtonToPickSection() {
+        if (storage.length > 0 && !document.getElementById("favouriteButton")) {
+            document.getElementById("pick-section").appendChild(Creator.createFavouriteRadioButton());
+            Adder.addEventListenersForPickSection();
         }
-        ).then(() => isLoading = false);
-
-}
-
-function removeDataIfExist() {
-    let listOfGifs = document.querySelectorAll("#content .gif");
-    listOfGifs.forEach(element => {
-        content.removeChild(element);
-    });
-}
-
-function removeDataIfExistFromDropDown() {
-    let listOfGifs = document.querySelectorAll("#dropdown .gif");
-    listOfGifs.forEach(element => {
-        dropDown.removeChild(element);
-    });
-}
-
-function createSmallGif(gifObject) {
-    let gif = document.createElement("div");
-    gif.className = "gif";
-    gif.setAttribute("gifid", gifObject.id);
-    let header = document.createElement("div");
-    header.className = "header";
-    let h5 = document.createElement("h5");
-    h5.innerText = gifObject.title;
-    let star = document.createElement("i");
-    star.classList.add("fa-star");
-    if (storage.find(element => element.id == gifObject.id)) {
-        star.classList.add("fas");
-    } else {
-        star.classList.add("far");
-    }
-    star.addEventListener("click", handleClickFavoriteButton);
-    header.appendChild(h5);
-    header.appendChild(star);
-    gif.appendChild(header);
-    let image = document.createElement("img");
-    if (gifObject.images) {
-        image.setAttribute("src", gifObject.images.fixed_height_small.url);
-    } else {
-        image.setAttribute("src", gifObject.url);
     }
 
-    gif.appendChild(image);
-    return gif;
-}
-
-function createGif(gifObject) {
-    let gif = document.createElement("div");
-    gif.className = "gif";
-    gif.setAttribute("gifid", gifObject.id);
-    let header = document.createElement("div");
-    header.className = "header";
-    let h5 = document.createElement("h5");
-    h5.innerText = gifObject.title;
-    let star = document.createElement("i");
-    star.classList.add("fa-star");
-    if (storage.find(element => element.id == gifObject.id)) {
-        star.classList.add("fas");
-    } else {
-        star.classList.add("far");
-    }
-    star.addEventListener("click", handleClickFavoriteButton);
-    header.appendChild(h5);
-    header.appendChild(star);
-    gif.appendChild(header);
-    let image = document.createElement("img");
-    if (gifObject.images) {
-        image.setAttribute("src", gifObject.images.downsized.url);
-    } else {
-        image.setAttribute("src", gifObject.url);
+    static addItemToStorage(item) {
+        storage.unshift(resultOfRequest.find(element => element.id == item.getAttribute("gifid")));        
+        localStorage.setItem("favourites", JSON.stringify(storage));
     }
 
-    gif.appendChild(image);
-    return gif;
+    static addEventListenersForPickSection() {
+        document.getElementsByName("contentType").
+            forEach(element =>
+                element.addEventListener("change", (event) => Handlers.handleChangePickContentType(event)));
+    }
+
+    static addEventListenerForSearchInput() {
+        searchInput.addEventListener("input", (event) => Handlers.handleInputChange(event));
+    }
+
+    static addEventListenerForSearchButton() {
+        document.getElementById("search-button").addEventListener("click", (event) => Handlers.handleClickSearchButton(event));
+    }
+
+    static addEventListenersForWindow() {
+        window.addEventListener("click", (event) => Handlers.handleClickNotOnDropDown(event));
+        window.addEventListener("scroll", (event) => Utils.checkForNewDiv());
+    }
+
 }
+
+class Handlers {
+
+    static handleClickNotOnDropDown(event) {
+        if (Utils.getParentNode(event.target) != dropDown) {
+            Eraser.removeDataIfExist("dropdown");
+        }
+    }
+
+
+    static handleInputChange(event) {
+        const request = event.target.value;
+        Eraser.removeDataIfExist("dropdown");
+        Utils.getData(request, "dropdown");
+    }
+
+    static handleClickFavoriteButton(event) {
+        const targetItem = event.target.parentNode.parentNode,
+            targetClassList = event.target.classList;
+        if (Utils.getParentNode(event.target) == dropDown) {
+            searchInput.focus();
+        }        
+        if (targetClassList.contains("far")) {
+            targetClassList.remove("far");
+            targetClassList.add("fas");
+            Adder.addItemToStorage(targetItem);
+            Adder.addFavouriteButtonToPickSection();
+        } else {
+            targetClassList.remove("fas");
+            targetClassList.add("far");
+            Eraser.deleteItemFromStorage(targetItem);
+            Eraser.deleteFavouriteRadioButton();
+        }
+    }
+
+    static handleChangePickContentType(event) {
+        offset = 0;
+        section = event.target.value;
+        Eraser.removeDataIfExist("content");
+        Utils.getData(searchInput.value, "content");
+    }
+
+    static handleClickSearchButton(event) {
+        Eraser.removeDataIfExist("content");
+        Eraser.removeDataIfExist("dropdown");
+        if (searchInput.value) {
+            Utils.getData(searchInput.value, "content");
+        }
+    }
+
+}
+
+class Creator {
+
+    static createFavouriteRadioButton() {
+        let label = document.createElement("label");
+        label.innerHTML = "<input type=\"radio\" id=\"favouriteButton\" name=\"contentType\" value=\"favourite\" />Избранное";
+        return label;
+    }
+
+    static createListOfEntities(data, place) {
+        switch (place) {
+            case "content": {
+                data.forEach(element => {
+                    content.appendChild(Creator.createEntity(element, "downsized"));
+                });
+                break;
+            }
+            case "dropdown": {
+                data.forEach(element => {
+                    dropDown.appendChild(Creator.createEntity(element, "fixed_height_small"));
+                });
+                break;
+            }
+        }
+    }
+
+    static createImageForEntity(entity, imageProperty) {
+        let image = document.createElement("img");
+        image.setAttribute("src", entity.images[imageProperty].url);
+        return image;
+    }
+
+    static createHeaderForEntity(title, id) {
+        let header = document.createElement("div");
+        header.className = "header";
+        header.appendChild(Creator.createH5ForEntity(title));
+        header.appendChild(Creator.createFavouriteStarForEntity(id));
+        return header;
+    }
+
+    static createH5ForEntity(title) {
+        let h5 = document.createElement("h5");
+        h5.innerText = title;
+        return h5;
+    }
+
+    static createFavouriteStarForEntity(id) {
+        let star = document.createElement("i");
+        star.classList.add("fa-star");
+        if (storage.find(element => element.id == id)) {
+            star.classList.add("fas");
+        } else {
+            star.classList.add("far");
+        }
+        star.addEventListener("click", (event) => Handlers.handleClickFavoriteButton(event));
+        return star;
+    }
+
+    static createEntity(entity, imageProperty) {
+        let gif = document.createElement("div");
+        gif.className = "gif";
+        gif.setAttribute("gifid", entity.id);
+        gif.appendChild(Creator.createHeaderForEntity(entity.title, entity.id));
+        gif.appendChild(Creator.createImageForEntity(entity, imageProperty));
+        return gif;
+    }
+}
+
+Adder.addAllListeners();    
